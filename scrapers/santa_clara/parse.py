ef76dd151a311e521ed83bd4917506a8d0d9b217
Ian Webster <ianw_github@ianww.com>
2020-10-07 09:45:20 -0700

    Speed up with regex for parsing

--- scrapers/santa_clara/parse.py
+++ scrapers/santa_clara/parse.py
@@ -5,12 +5,15 @@ import csv
 import gzip
 import json
 import os
+import re
 import sys
 
 from shapely.geometry import Polygon
-from bs4 import BeautifulSoup
 
 csv.field_size_limit(sys.maxsize)
+
+ADDRESS_REGEX = re.compile('name="situsLine1" type="hidden" value="(.*)"')
+AMOUNT_REGEX = re.compile('amountduecolumn.*\n\s+\$([\d,\.]+)')
 
 with open('/home/ian/Downloads/Santa_Clara_Parcels.csv') as f_in, \
      open('./parse_output.csv', 'w') as f_out:
@@ -27,9 +30,7 @@ with open('/home/ian/Downloads/Santa_Clara_Parcels.csv') as f_in, \
 
         lnglats = []
         geom_str = record['the_geom'].replace('MULTIPOLYGON (((', '').replace(')))', '')
-        #print('geomstr', geom_str)
         geom_splits = map(lambda lnglatstr: lnglatstr.strip().split(' '), geom_str.replace('(', '').replace(')', '').split(','))
-        #print('splits', geom_splits)
         coords = [(float(x[0]), float(x[1])) for x in geom_splits]
 
         try:
@@ -47,21 +48,19 @@ with open('/home/ian/Downloads/Santa_Clara_Parcels.csv') as f_in, \
         with gzip.open(output_path, 'rt') as f_in:
             html = f_in.read()
 
-        soup = BeautifulSoup(html, 'html.parser')
         try:
-            address = soup.find(id='situsLine1').get('value')
-        except AttributeError:
+            address = ADDRESS_REGEX.search(html).group(1) + ''
+        except:
             print('--> No address')
             continue
         print('--> address', address)
 
         amount = -1
-        amount_elt = soup.find('div', class_='amountduecolumn')
-        amount_str = amount_elt.get_text().replace('$', '').replace(',', '')
         try:
-            amount = float(amount_str)
+            amount = float(AMOUNT_REGEX.search(html).group(1).replace(',', ''))
         except:
             print('--> Could not parse float', amount_str)
+            continue
 
         print('--> Paid', amount)
 
@@ -72,4 +71,3 @@ with open('/home/ian/Downloads/Santa_Clara_Parcels.csv') as f_in, \
             'longitude': centroid[0],
             'tax': amount * 2,
         })
-        #f_out.flush()
