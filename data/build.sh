f367fe4c717f819d9b92d4567ca58a9f2970f6b3
Ian Webster <ianw_github@ianww.com>
2020-10-21 16:55:52 -0700

    Add Sacramento County

--- data/build.sh
+++ data/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 # Note: Santa Cruz has to come first due to some weird formatting issue
-cat santa_cruz_2017.2.csv.gz santa_clara.csv.gz san_mateo.csv.gz san_francisco.csv.gz alameda.csv.gz contra_costa.csv.gz sonoma.csv.gz solano.csv.gz yolo.csv.gz marin.csv.gz los_angeles.csv.gz san_bernadino.csv.gz > ca_all.csv.gz
+cat santa_cruz_2017.2.csv.gz santa_clara.csv.gz san_mateo.csv.gz san_francisco.csv.gz alameda.csv.gz contra_costa.csv.gz sonoma.csv.gz solano.csv.gz yolo.csv.gz marin.csv.gz los_angeles.csv.gz san_bernadino.csv.gz sacramento.csv.gz > ca_all.csv.gz
