de1638d6a7f01b1ff23c8645b059a58050ff4cdd
Kevin Buchanan <kevaustinbuch@gmail.com>
2020-10-21 21:32:23 -0700

    Add SLO County

---
+++ scrapers/san_luis_obispo/README.md
@@ -0,0 +1,27 @@
+## Parcel List
+
+https://opendata.slocounty.ca.gov/datasets/planning-land-use-by-parcel/data
+
+The parcel list gets us a list of APNs, but doesn't include addresses, coordinates, or tax data.
+
+`curl https://opendata.arcgis.com/datasets/04535c05c1d5413b9896567cec36346e_83.geojson --output datasets/parcels.geojson`
+
+## Address / Coordinate List
+
+https://opendata.slocounty.ca.gov/datasets/address-points/data
+
+The address list doesn't include APN, but should be an exact match for full address provided in the assessment.
+
+`curl https://opendata.arcgis.com/datasets/61f3ddf2074942e6932b8f5084c20ab4_0.geojson --output datasets/addresses.geojson`
+
+## Property Information Search, `scrape_output/<apn>.assessment.html`
+
+https://assessor.slocounty.ca.gov/assessor/pisa/Search.aspx
+
+The assessor's search is necessary to tie APN to address.
+
+## Property Tax Search, `scrape_output/<apn>.tax.html`
+
+https://services.slocountytax.org/Entry.aspx
+
+The tax search will provide 2020 property taxes for an APN.
---
+++ scrapers/san_luis_obispo/parse.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+import csv
+import gzip
+import os
+import re
+import sys
+import json
+
+from bs4 import BeautifulSoup
+from shapely.geometry import Polygon
+
+csv.field_size_limit(sys.maxsize)
+
+AMOUNT_REGEX = re.compile('Total Amount Due:\s+<span class="i16">\$([\d,\.]+)')
+
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets/parcels.geojson')) as apn_in, \
+     open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'parse_output.csv'), 'w') as f_out:
+    fieldnames = ['address', 'apn', 'longitude', 'latitude', 'tax', 'county']
+    writer = csv.DictWriter(f_out, fieldnames=fieldnames)
+
+    count = 0
+    for line in apn_in:
+        count += 1
+        if count < 6:
+            # Skip geojson garbage
+            continue
+
+        try:
+            record = json.loads(line[:-2])
+        except:
+            continue
+
+        if 'APN' not in record['properties']:
+            print('-> no APN')
+            continue
+        apn = record['properties']['APN']
+        if not apn:
+            print('-> blank APN')
+            continue
+
+        print(count, apn)
+
+        assessment_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'scrape_output/%s.assessment.html' % (apn))
+        if not os.path.exists(assessment_path):
+            continue
+
+        try:
+            with gzip.open(assessment_path, 'rt') as f_in:
+                html = f_in.read()
+        except:
+            print('--> bad file')
+            continue
+
+        soup = BeautifulSoup(html, 'html.parser')
+        address = soup.find(id='Main_lblAddressValue').text
+
+        if address == ' ':
+            continue
+
+        print('--> Address', address)
+
+        tax_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'scrape_output/%s.tax.html' % (apn))
+        if not os.path.exists(assessment_path):
+            continue
+
+        try:
+            with gzip.open(tax_path, 'rt') as f_in:
+                html = f_in.read()
+        except:
+            print('--> bad file')
+            continue
+
+        soup = BeautifulSoup(html, 'html.parser')
+        amount_str = soup.find(id='SecTaxBills_RadListView1_ctrl0_lblSecondInstallAmt').text.replace(',', '').replace('$', '')
+
+        try:
+            amount = float(amount_str)
+        except:
+            print('--> Could not parse amount', amount_str)
+            continue
+
+        print('--> Paid', amount)
+
+        address_search = address.upper()
+        address_match = None
+
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets/addresses.geojson')) as address_in:
+            for line in address_in:
+                if address_search in line:
+                    address_match = json.loads(line[:-2])
+
+            if not address_match:
+                print('--> No Address')
+                continue
+
+            coords = address_match['geometry']['coordinates']
+
+        writer.writerow({
+            'address': address,
+            'apn': apn,
+            'latitude': coords[1],
+            'longitude': coords[0],
+            'tax': amount,
+            'county': 'SLO',
+        })
