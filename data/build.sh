7bda2d2a7b959a955e0804123359d7130a450ef6
Ian Webster <ianw_github@ianww.com>
2020-10-27 08:41:48 -0700

    Add san diego handler

--- data/build.sh
+++ data/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-cat santa_cruz.csv.gz santa_clara.csv.gz san_mateo.csv.gz san_francisco.csv.gz alameda.csv.gz contra_costa.csv.gz napa.csv.gz sonoma.csv.gz solano.csv.gz yolo.csv.gz marin.csv.gz napa.csv.gz los_angeles.csv.gz san_bernadino.csv.gz sacramento.csv.gz san_luis_obispo.csv.gz > ca_all.csv.gz
+cat san_diego.csv.gz  santa_cruz.csv.gz santa_clara.csv.gz san_mateo.csv.gz san_francisco.csv.gz alameda.csv.gz contra_costa.csv.gz napa.csv.gz sonoma.csv.gz solano.csv.gz yolo.csv.gz marin.csv.gz napa.csv.gz los_angeles.csv.gz san_bernadino.csv.gz sacramento.csv.gz san_luis_obispo.csv.gz > ca_all.csv.gz
