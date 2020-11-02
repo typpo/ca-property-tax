bb4072fddfd128d288ab6808049369c4d268059b
Ian Webster <ianw_github@ianww.com>
2020-11-02 08:44:38 -0800

    add fresno and update csv build script

--- data/build.sh
+++ data/build.sh
@@ -1,3 +1,24 @@
 #!/bin/bash -e
 
-cat san_diego.csv.gz  santa_cruz.csv.gz santa_clara.csv.gz san_mateo.csv.gz san_francisco.csv.gz alameda.csv.gz contra_costa.csv.gz napa.csv.gz sonoma.csv.gz solano.csv.gz yolo.csv.gz marin.csv.gz napa.csv.gz los_angeles.csv.gz san_bernadino.csv.gz sacramento.csv.gz san_luis_obispo.csv.gz > ca_all.csv.gz
+files=(
+  san_diego.csv.gz
+  santa_cruz.csv.gz
+  santa_clara.csv.gz
+  san_mateo.csv.gz
+  san_francisco.csv.gz
+  alameda.csv.gz
+  contra_costa.csv.gz
+  napa.csv.gz
+  sonoma.csv.gz
+  solano.csv.gz
+  yolo.csv.gz
+  marin.csv.gz
+  napa.csv.gz
+  los_angeles.csv.gz
+  san_bernadino.csv.gz
+  sacramento.csv.gz
+  san_luis_obispo.csv.gz
+  fresno.csv.gz
+)
+
+cat "${files[@]}" > ca_all.csv.gz
